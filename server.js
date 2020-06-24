93c178bd0708bf817c34c59c4955059096f01fad
jasonleyser <jason.leyser@protocol.ai>
2020-06-23 23:31:20 -0700

    working example

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
 import express from "express";
+import fs from "fs"
+import { ffs, createPow } from "@textile/powergate-client"
 
-const dev = process.env.NODE_ENV !== "production";
-const port = process.env.PORT || 8080;
+const pow = createPow({ host:"http://0.0.0.0:6002" })
+const server = express();
 
-  const server = express();
+server.listen(8080, async () => {
 
-  if (!dev) {
-    server.use(compression());
-  }
+  const { token } = await pow.ffs.create() // save this token for later use!
+  console.log({token});
+  pow.setToken(token)
 
-
-  server.get("*", async (req, res) => {
-    return nextRequestHandler(req, res, req.url);
-  });
-
-  server.listen(port, (err) => {
-    if (err) {
-      throw err;
+  const { cid } = await pow.ffs.addToHot(buffer)
+  console.log({cid});
+  // store the data in FFS using the default storage configuration
+  const { jobId } = await pow.ffs.pushConfig(cid)
+  console.log({jobId});
+  // cache data in IPFS in preparation to store it using FFS
+  const buffer = fs.readFileSync(`dog.jpg`)
+  // watch the FFS job status to see the storage process progressing
+  const cancel = pow.ffs.watchJobs((job) => {
+    console.log({job})
+    if (job.status === ffs.JobStatus.CANCELED) {
+      console.log("job canceled")
+    } else if (job.status === ffs.JobStatus.FAILED) {
+      console.log("job failed")
+    } else if (job.status === ffs.JobStatus.SUCCESS) {
+      console.log("job success!")
     }
-
-    console.log(`[ server ]: http://localhost:${port}`);
+  }, jobId);
 });
